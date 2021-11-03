cfab6664905090c2040cc26787489ec1eb863bf6
Chaitanya Singhal <82356334+Chaitanya0909@users.noreply.github.com>
2021-11-03 13:33:06 +0530

    Add files via upload
    Basics

---
+++ Bollywood/09_Bollywood_Exercise.R
@@ -0,0 +1,92 @@
+ 
+#	Import the Bollywood data set in Rstudio in a variable named bollywood
+## (Remember to set working directory)
+
+
+bollywood <- 
+
+  
+View(bollywood)
+
+#	When you import a data set, You can check the structure of the data frame by using str()
+
+
+
+# Import dataset such that strings are read in character form.
+
+#Q1.
+#	Access the last 10 movies (from the bottom of the Bollywood data frame) using column bollywood$Movie
+# Store the names of those movies in last_10 vector (in the same order)
+     
+last_10 <- 
+  
+#Q2.
+#	Find out the total number of  missing values (NA) in the bollywood data frame.
+# Store the result in na_bollywood vector
+     
+na_bollywood <- 
+  
+
+#Q3
+#	Write the command(s) to find out which movie tops the list in terms of Total Collections
+# Store the movie name in variable named top_movie
+
+
+top_movie <- 
+  
+
+#Q4
+#	Write the command to find out which movie comes second on the list in terms of Total Collections
+# Store the movie name in variable named top_2_movie
+
+top_2_movie <- 
+  
+# Now let's find out the movies shot by Shahrukh, Akshay and Amitabh separately.
+
+shahrukh <- 
+
+akshay <- 
+
+amitabh <- 
+
+
+#Q5
+#	What is the total collection of Shahrukh, Akshay and Amitabh movies individually?
+# You can use	a column named 'Tcollection' for this
+ 
+shahrukh_collection <- 
+    
+akshay_collection <- 
+    
+amitabh_collection <- 
+   
+shahrukh_collection ; akshay_collection ; amitabh_collection
+	
+
+
+#Q6  
+# Write command/s to find out how many movies are in Flop, Average, Hit and 
+# Superhit categories in the entire Bollywood data set.
+
+
+
+#Q7 Write a command to find the maximum value of Ocollection, Wcollection, 
+#   Fwcollection and Tcollection
+#   Remember! You can't write many commands; one for each result.
+#   All results should appear in One-Go.
+
+
+
+
+#Q8 
+# Write a command to find the names of the movies which have the maximum Ocollection, 
+# Wcollection, Fwcollecion & Tcollection.
+# Store the names of 4 movies in same sequence in movie_result vector.
+#   Remember! You can't write many commands; one for each result.
+#   All results should appear in One-Go.
+
+movie_result <- 
+
+
+
+#############################
